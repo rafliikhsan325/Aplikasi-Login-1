db0dc7f692deb32cd90ef47baddb1a3c6d48b598
rafliikhsan325 <rafliikhsan325@gmail.com>
2026-02-13 09:32:32 +0700

    Enhance documentation in login-script.js
    Mengganti komentar sesuai dengan format JSDOc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,15 +1,41 @@
-/*
+m/*
  * Catatan:
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
-
+/** @module Login-Script */
+/**
+*Membuat variabel loginformElement untuk tampilan form.
+*@constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/** @module Login-Script */
+/**
+*Membuat variabel inputEmailElement untuk tampilan input email.
+*@constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/**
+*Membuat variabel inputpasswordElement untuk tampilan input password
+*@constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+*Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+*@constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+*Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+*@constant {string}
+*/
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
